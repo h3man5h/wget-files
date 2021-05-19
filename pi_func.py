a1eacad7e7eb84b69e22e575c3a4a3101f9e3b21
h3man5h <81043775+h3man5h@users.noreply.github.com>
2021-05-19 22:43:59 +0100

    Update pi_func.py
    Arguments have been passed

--- pi_func.py
+++ pi_func.py
@@ -1,18 +1,22 @@
 import math
 import random
 
-shots = 10000
-value = 20
-output=[]
+def pi(shots, value):
+    # shots = 10000
+    # value = 20
+    output=[]
 
-for i in range(0, value):
-    incircle = 0
-    for i in range(0, shots):
-        random1 = random.uniform(-1.0, 1.0)
-        random2 = random.uniform(-1.0, 1.0)
-        if( ( random1*random1 + random2*random2 ) < 1 ):
-          incircle += 1
+    for i in range(0, value):
+        incircle = 0
+        for i in range(0, shots):
+            random1 = random.uniform(-1.0, 1.0)
+            random2 = random.uniform(-1.0, 1.0)
+            if( ( random1*random1 + random2*random2 ) < 1 ):
+              incircle += 1
 
-    y=(4.0 * incircle/shots)
-    output.append((incircle,shots,y))
-print(output)
+        y=(4.0 * incircle/shots)
+        output.append((incircle,shots,y))
+    return(output)
+
+if __name__== "__main__":
+    pi(int(sys.argv[1]), int(sys.argv[2]))
